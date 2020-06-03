1abfd2a36fef0de98090c9655e94c244c1b5f32a
awsmaksym25 <aws.maksym25@gmail.com>
2020-06-03 09:50:43 +0300

    skip_cleanup: true

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+echo 'shemetmaksim/zabbix:"$TRAVIS_COMMIT" should be deployed...' 
+
+
 if [ $(gcloud compute instances list |  grep "zabbix" | wc -l) -eq 0 ];
 then
     gcloud compute instances create-with-container zabbix --zone $ZONE --tags=http-server --container-image shemetmaksim/zabbix:"$TRAVIS_COMMIT"
